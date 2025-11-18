5e705396122f84eec6b233cb01283de09b6833cd
arpan7sarkar <arpansakar@gmail.com>
2025-11-18 19:14:25 +0530

    working with constrains

--- GENERICS (Deep)/genericInterface.ts
+++ GENERICS (Deep)/genericInterface.ts
@@ -18,3 +18,17 @@ products:[{name:"Mobile",price:12000},{name:"Laptop",price:72000},{name:"Earphon
   isAvilable:[true,true,false]
 }
 console.log(products);
+
+
+// 6. Create a generic constraint:
+
+//   Accept only objects with a `name` key
+
+interface Mandtory{
+  name:string;
+}
+function nameOnly<T extends Mandtory>(arg:T):T{
+  return arg;
+}
+const func=nameOnly<{name:string,y:number}>({name:"hello",y:4})
+const func2=nameOnly<{name:string}>({name:"hello"})
